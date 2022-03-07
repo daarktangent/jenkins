81d57df79fd3e66dfa614017b281dafc139be278
aman kumar jha <97963877+daarktangent@users.noreply.github.com>
2022-03-07 16:00:44 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -2,9 +2,3 @@
 #The above lie is important to build python program 
 print("Hello from new file \n")
 print("Hello from new file \n")
-print("Hello from new file \n")
-print("Hello from new file \n")
-print("Hello from new file \n")
-print("Hello from new file \n")
-print("Hello from new file \n")
-
